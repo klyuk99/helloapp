aa26f478577e17b874180fa56caadd6b381e5914
white <white@example.com>
2026-07-13 04:24:12 +0500

    Add new method GreetWhite

--- Program.cs
+++ Program.cs
@@ -3,5 +3,9 @@
         public static void Main() {
             Console.WriteLine("hello");
         }
+
+        public static void GreetWhite() {
+            Console.WriteLine("hello white");
+        }
     }
 }
